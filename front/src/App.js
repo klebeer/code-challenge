d55834b21068d022eefa95f90a113ee396ef9ade
Kleber Ayala <kleber.ayala@gmail.com>
2023-07-09 17:54:33 -0500

    fix: bootstrap css

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import FileDataContainer from './containers/FileDataContainer';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return <FileDataContainer />;
 }
